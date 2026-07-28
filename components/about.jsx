c2e61e04916e0e3bd6d06268442068f4c9d6b94b
Boboinna <andrelle364808@gmail.com>
2026-07-29 01:10:47 +0300

    test final

--- components/about.jsx
+++ components/about.jsx
@@ -1,7 +1,7 @@
 export default function About({ image, about }) {
   return (
     <aside>
-      <img src={image} alt="Blog logo" class="img" />
+      <img src={image} alt="blog logo" class="img" />
       <p>{about}</p>
     </aside>
   )
